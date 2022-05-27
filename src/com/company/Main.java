b7aae2b381de45484ba4c181c9504b352dc21b09
ss210344 <ss210344@reigate.ac.uk>
2022-05-27 12:12:14 +0100

    added board display

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,14 +5,8 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
         pawn[][] board = boardCreation.makeBoard(boardCreation.getBoardSize());
-        boardCreation.initialiseBoard(board);
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                pawn temp = board[i][j];
-                System.out.println(temp.getColour());
-            }
+        board = boardCreation.initialiseBoard(board);
+        boardDisplay.displayBoard(board);
 
-
-        }
     }
 }
---
+++ src/com/company/boardDisplay.java
@@ -0,0 +1,37 @@
+package com.company;
+
+import com.sun.xml.internal.ws.addressing.WsaActionUtil;
+
+public class boardDisplay {
+    public static void displayBoard(pawn[][] board){
+        // top line
+        System.out.print("-----");
+        for (int i = 0; i < board.length; i++) {
+            int numLetter = i+65;
+            char letter = (char) numLetter;
+            System.out.print("--"+letter+"--");
+        }
+        System.out.println("");
+        for (int i = 0; i < board.length; i++) {
+            System.out.print("--"+i+"--");
+            for (int j = 0; j < board[i].length; j++) {
+
+                pawn temp = board[i][j];
+                if (temp.getColour()=="blank"){
+                    System.out.print(" [ ] ");
+                }
+                if (temp.getColour()=="black"){
+                    System.out.print(" [B] ");
+                }
+                if (temp.getColour()=="white"){
+                    System.out.print(" [W] ");
+                }
+
+            }
+            System.out.println("");
+
+
+        }
+    }
+
+}
