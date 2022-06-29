c67efe5aa641d189154192e5ce4e1f815d221a90
ss210344 <ss210344@reigate.ac.uk>
2022-06-29 11:52:53 +0100

    added way to see win record

---
+++ src/com/company/WinRecord.java
@@ -0,0 +1,38 @@
+package com.company;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class WinRecord {
+    public static void SeeWinRecord(String gameTypeToGet){
+
+        String DatabaseLocation = System.getProperty("user.dir") + "\\NEA_HexaPawn.accdb";
+
+        try {
+            Connection con = DriverManager.getConnection("jdbc:ucanaccess://" + DatabaseLocation, "", "");
+            Statement stmt = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+            String sql = "SELECT * FROM WinRecord";
+            ResultSet rs = stmt.executeQuery(sql);
+            while(rs.next()) {
+                String GameType = rs.getString("GameType");
+                if(GameType.equals(gameTypeToGet)){
+                    System.out.println("Board size : "+ GameType.substring(0,3) );
+                    System.out.println("Play played as :"+GameType.substring(3,GameType.length()) );
+                    System.out.println("Player Wins : " +rs.getInt("PlayerWins"));
+                    System.out.println("AI wins : " + rs.getInt("AiWins"));
+                    break;
+                }
+
+
+            }
+            rs.close();
+            con.close();
+        } catch (Exception e) {
+            System.out.println("Error in the SQL class SeeWinRecord: " + e);
+        }
+    }
+}
+
+
