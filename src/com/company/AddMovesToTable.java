66e0d4f2101e36a6ec3e3dca4c9aaa7ca588d15e
ss210344 <ss210344@reigate.ac.uk>
2022-08-24 14:09:21 +0100

    added get board id

--- src/com/company/AddMovesToTable.java
+++ src/com/company/AddMovesToTable.java
@@ -7,22 +7,22 @@ import java.sql.Statement;
 import java.util.ArrayList;
 
 public class AddMovesToTable {
-    public static void AddMoves(ArrayList<String>ListOfLegalMoveCodes,pawn[][]board){
-        String TableBaseName = board.length +"x" +board[0].length;
+    public static void AddMoves(ArrayList<String> ListOfLegalMoveCodes, pawn[][] board) {
+        String TableBaseName = board.length + "x" + board[0].length;
 
         //make board fennel string
         String boardState = "";
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
                 pawn temp = board[i][j];
-                if (temp.getColour().equals("blank")){
-                    boardState=boardState+"00";
+                if (temp.getColour().equals("blank")) {
+                    boardState = boardState + "00";
                 }
-                if (temp.getColour().equals("black")){
-                    boardState=boardState+"10";
+                if (temp.getColour().equals("black")) {
+                    boardState = boardState + "10";
                 }
-                if (temp.getColour().equals("white")){
-                    boardState=boardState+"01";
+                if (temp.getColour().equals("white")) {
+                    boardState = boardState + "01";
 
 
                 }
@@ -30,23 +30,31 @@ public class AddMovesToTable {
         }
         int BoardStateNumber = binaryToInteger(boardState);
         //see if board is in the DataBase
-        if (!isBoardInDataBase(TableBaseName,BoardStateNumber)){
+        if (!isBoardInDataBase(TableBaseName, BoardStateNumber)) {
             //add board to database
-            AddBoardToDataBase(TableBaseName,BoardStateNumber);
+            AddBoardToDataBase(TableBaseName, BoardStateNumber);
         }
 
         //get list of move already in dataBAse
-        System.out.println(isBoardInDataBase(TableBaseName,BoardStateNumber));
-        ArrayList<String>ListOfMoveCodesInDataBase = GetListOFMoveCodesInDataBase(TableBaseName);
+        System.out.println(isBoardInDataBase(TableBaseName, BoardStateNumber));
+        ArrayList<String> ListOfMoveCodesInDataBase = GetListOFMoveCodesInDataBase(TableBaseName);
         // remove move code already in database
-        for (int i = 0; i < ListOfMoveCodesInDataBase.size(); i++) {
-            System.out.println(ListOfLegalMoveCodes.get(i));
+        ArrayList<String> ListOfMoveCodesNotRepeated = removeMoveCodeAlreadyInDataBase(ListOfLegalMoveCodes, ListOfMoveCodesInDataBase);
+        //add move codes not in database
+        for (int i = 0; i < ListOfMoveCodesNotRepeated.size(); i++) {
+            AddMoveCodesToDataBase(TableBaseName, ListOfMoveCodesNotRepeated.get(i));
+        }
+        //link the moves and table
+        int BoardID = GetBoardID(TableBaseName, BoardStateNumber);
+        for (int i = 0; i < ListOfLegalMoveCodes.size(); i++) {
+            int MoveID = 1;
+            AddLinkToDataBase(TableBaseName, BoardID, MoveID);
+
         }
 
 
-
-
     }
+
     private static int binaryToInteger(String binary) {
         char[] numbers = binary.toCharArray();
         int result = 0;
@@ -58,14 +66,14 @@ public class AddMovesToTable {
         return result;
     }
 
-    private static boolean isBoardInDataBase(String baseTableName,int BoardStateNumber) {
+    private static boolean isBoardInDataBase(String baseTableName, int BoardStateNumber) {
 
         String DatabaseLocation = System.getProperty("user.dir") + "\\NEA_HexaPawn.accdb";
         Boolean exist = false;
         try {
             Connection con = DriverManager.getConnection("jdbc:ucanaccess://" + DatabaseLocation, "", "");
             Statement stmt = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
-            String sql = "SELECT * FROM BoardState "+ baseTableName +";";
+            String sql = "SELECT * FROM BoardState " + baseTableName + ";";
             ResultSet rs = stmt.executeQuery(sql);
             while (rs.next()) {
                 rs.close();
@@ -80,18 +88,18 @@ public class AddMovesToTable {
 
     }
 
-    private static void AddBoardToDataBase(String baseTableName,int BoardStateNumber) {
+    private static void AddBoardToDataBase(String baseTableName, int BoardStateNumber) {
         System.out.println(baseTableName);
 
         String DatabaseLocation = System.getProperty("user.dir") + "\\NEA_HexaPawn.accdb";
         try {
             Connection con = DriverManager.getConnection("jdbc:ucanaccess://" + DatabaseLocation, "", "");
             Statement stmt = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
-            String sql = "INSERT INTO BoardState"+baseTableName +" (BoardFennelString,BoardSize)";
-            sql =sql + " Values("+BoardStateNumber+","+baseTableName+");";
+            String sql = "INSERT INTO BoardState" + baseTableName + " (BoardFennelString,BoardSize)";
+            sql = sql + " Values(" + BoardStateNumber + "," + baseTableName + ");";
             System.out.println(sql);
             int i = stmt.executeUpdate(sql);
-            System.out.println("rows Added "+i);
+            System.out.println("rows Added " + i);
             con.close();
 
 
@@ -99,16 +107,17 @@ public class AddMovesToTable {
             System.out.println("Error in the SQL class: AddBoardToDataBase " + e);
         }
     }
+
     private static ArrayList<String> GetListOFMoveCodesInDataBase(String baseTableName) {
 
         String DatabaseLocation = System.getProperty("user.dir") + "\\NEA_HexaPawn.accdb";
-        ArrayList<String>ListOfMoveCodesInDataBase = new ArrayList<>();
+        ArrayList<String> ListOfMoveCodesInDataBase = new ArrayList<>();
         try {
             Connection con = DriverManager.getConnection("jdbc:ucanaccess://" + DatabaseLocation, "", "");
             Statement stmt = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
-            String sql = "SELECT * FROM Moves"+ baseTableName+";";
+            String sql = "SELECT * FROM Moves" + baseTableName + ";";
             ResultSet rs = stmt.executeQuery(sql);
-            while(rs.next()){
+            while (rs.next()) {
                 ListOfMoveCodesInDataBase.add(rs.getString("MoveCode"));
             }
             rs.close();
@@ -119,18 +128,19 @@ public class AddMovesToTable {
         }
         return ListOfMoveCodesInDataBase;
     }
-    private static ArrayList<String> removeMoveCodeAlreadyInDataBase(ArrayList<String>ListOfLegalMoveCodes ,ArrayList<String>ListOfMoveCodesInDataBase){
-        ArrayList<String>ListOfNonRepeatedMoveCodes = new ArrayList<>();
+
+    private static ArrayList<String> removeMoveCodeAlreadyInDataBase(ArrayList<String> ListOfLegalMoveCodes, ArrayList<String> ListOfMoveCodesInDataBase) {
+        ArrayList<String> ListOfNonRepeatedMoveCodes = new ArrayList<>();
         for (int i = 0; i < ListOfLegalMoveCodes.size(); i++) {
-            String MoveCode =ListOfLegalMoveCodes.get(i);
+            String MoveCode = ListOfLegalMoveCodes.get(i);
             Boolean repeated = false;
             for (int j = 0; j < ListOfMoveCodesInDataBase.size(); j++) {
-                if(MoveCode.equals(ListOfMoveCodesInDataBase.get(j))){
+                if (MoveCode.equals(ListOfMoveCodesInDataBase.get(j))) {
                     repeated = true;
                     break;
                 }
             }
-            if (repeated == false){
+            if (repeated == false) {
                 ListOfNonRepeatedMoveCodes.add(MoveCode);
             }
         }
@@ -138,8 +148,77 @@ public class AddMovesToTable {
 
     }
 
+    private static void AddMoveCodesToDataBase(String baseTableName, String MoveCode) {
+
+
+        String DatabaseLocation = System.getProperty("user.dir") + "\\NEA_HexaPawn.accdb";
+        try {
+            Connection con = DriverManager.getConnection("jdbc:ucanaccess://" + DatabaseLocation, "", "");
+            Statement stmt = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+            String sql = "INSERT INTO Moves" + baseTableName + " (MoveCode)";
+            sql = sql + " Values();";
+            System.out.println(sql);
+            int i = stmt.executeUpdate(sql);
+            System.out.println("rows Added " + i);
+            con.close();
+
+
+        } catch (Exception e) {
+            System.out.println("Error in the SQL class: AddMoveCodesToDataBase " + e);
+        }
+    }
+
+    private static void AddLinkToDataBase(String baseTableName, int BoardID, int MoveID) {
+        System.out.println(baseTableName);
+
+        String DatabaseLocation = System.getProperty("user.dir") + "\\NEA_HexaPawn.accdb";
+        try {
+            Connection con = DriverManager.getConnection("jdbc:ucanaccess://" + DatabaseLocation, "", "");
+            Statement stmt = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+            String sql = "INSERT INTO Link" + baseTableName + " (BoardID,MoveID)";
+            sql = sql + " Values(" + BoardID + "," + MoveID + ");";
+            System.out.println(sql);
+            int i = stmt.executeUpdate(sql);
+            System.out.println("rows Added " + i);
+            con.close();
+
+
+        } catch (Exception e) {
+            System.out.println("Error in the SQL class: AddBoardToDataBase " + e);
+        }
+    }
+
+    public static int GetBoardID(String baseTableName, int BoardFennelStringToGet) {
+
+        String DatabaseLocation = System.getProperty("user.dir") + "\\NEA_HexaPawn.accdb";
+
+        try {
+            Connection con = DriverManager.getConnection("jdbc:ucanaccess://" + DatabaseLocation, "", "");
+            Statement stmt = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+            String sql = "SELECT * FROM BoardState" + baseTableName + ";";
+            ResultSet rs = stmt.executeQuery(sql);
+            while (rs.next()) {
+                int BoardFennelString = rs.getInt("BoardFennelString");
+                if (BoardFennelString == BoardFennelStringToGet) {
+                    return rs.getInt("BoardID");
+                }
+
+            }
+            System.out.println("record not found");
+            rs.close();
+            con.close();
+        } catch (Exception e) {
+            System.out.println("Error in the SQL class SeeWinRecord: " + e);
+            return 0;
+        }
+        return 0;
+    }
+
 
 
 }
+
+
+
 
 
